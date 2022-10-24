dbdd8d3d62020c7ef0d10907c5dcc2ae1b553dcb
zengbinsreve <useremail@qq.com>
2022-10-24 20:57:19 +0800

    githooks

--- githooks/index.js
+++ githooks/index.js
@@ -1,13 +1,18 @@
 const express = require('express')
 const { exec } = require('child_process');
 const app = express()
+app.use(express.json())
 const port = 9997
 
 app.post('/', (req, res) => {
-  exec('cd /root/home/web/music-react && git pull && npm run build',(error, stdout, stderr)=>{
-    console.log(error, stdout, stderr)
-  })
-  res.send('success:' + new Date())
+  if (req.body.password == 'Mc2SajhBp5LTnJ') {
+    res.send('success: ' + new Date())
+
+    exec('cd /root/home/web/music-react && npm i && npm run build', (error, stdout, stderr) => {
+      console.log(error, stdout, stderr)
+    })
+
+  }
 })
 
 app.listen(port, () => {
