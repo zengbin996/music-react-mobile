e3ef74b9873829e88028e929cd23a91c5811f8fe
zengbin <zengb-b@glodon.com>
2023-02-04 15:44:25 +0800

    自述文件

--- src/request/http.js
+++ src/request/http.js
@@ -1,9 +1,10 @@
 import axios from 'axios'
+import { BASE_URL } from '../config'
 
 axios.defaults.timeout = 10000
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8;multipart/form-data'
 axios.defaults.withCredentials = true
-axios.defaults.baseURL = 'http://127.0.0.1:9998'
+axios.defaults.baseURL = BASE_URL
 
 axios.interceptors.response.use(
   function (response) {
